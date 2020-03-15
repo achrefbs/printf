58e317028a460976970b1afbb5d4bca79854a932
achrefbs <achrefbs@github.com>
2020-03-15 18:38:40 +0000

    created % checker

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 int _printf(const char *format, ...)
 {
-
+int i;
 specifiers_t specifiers[] = {
 {"c", print_char},
 {"s", print_string},
@@ -9,6 +9,22 @@ specifiers_t specifiers[] = {
 {NULL, NULL}
 };
 
+for (i = 0; format[i] != '\0'; i++)
+{
+	if (format[i] == '%')
+	{
+		if (format[i + 1] == (specifiers + i)-> dir)
+		{
+			(specifiers + i)->f();
+		}
+		else
+		{
+			print_char(format[i]);
+		}
+		
+	}
+	
+}
 
 
 
