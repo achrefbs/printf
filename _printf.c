eb47944765b3305e7fd79ce184bd9e7642b11d6b
achrefbs <achrefbs@github.com>
2020-03-16 18:39:17 +0000

    added format NULL conditon

--- _printf.c
+++ _printf.c
@@ -8,6 +8,8 @@ specifiers_t specifiers[] = {
 {'s', print_string},
 };
 va_start(argslist, format);
+if (!format)
+return (-1);
 for (i = 0; format[i] != '\0'; i++)
 {
 	if (format[i] == '%')
@@ -31,8 +33,7 @@ for (i = 0; format[i] != '\0'; i++)
 			_putchar(format[i]);
 			l += 1;
 		}
-	if (!format && !format[i + 1])
-	  return (-1);
+
 }
 va_end(argslist);
 return (l);
