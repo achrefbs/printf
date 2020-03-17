88999be324dd3e99da0bb5d8c3d50b530cf01c00
vagrant <vagrant@homestead.test>
2020-03-17 22:49:37 +0000

    update betty

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 int _printf(const char *format, ...)
 {
 va_list argslist;
@@ -14,32 +15,30 @@ specifiers_t specifiers[] = {
 if (format == NULL)
 return (-1);
 va_start(argslist, format);
-
 for (i = 0; format[i] != '\0'; i++)
 {
-  if(format[i] == '%')
-  	{
-    i++;
-    if (format[i] == '\0')
-    return (-1);
-    if (format[i] == '%')
-    {
-    _putchar(format[i]);
-    l++;
-    }
-    for (j = 0; (specifiers + j)->dir != NULL; j++)
-    {
-      if (format[i] == *(specifiers + j)->dir)
-      l += (specifiers + j)->f(argslist);
-    }
-	}
-	else
-	{
-		_putchar(format[i]);
-		l++;
-	}
+if(format[i] == '%')
+{
+i++;
+if (format[i] == '\0')
+return (-1);
+if (format[i] == '%')
+{
+_putchar(format[i]);
+l++;
 }
-
+for (j = 0; (specifiers + j)->dir != NULL; j++)
+{
+if (format[i] == *(specifiers + j)->dir)
+l += (specifiers + j)->f(argslist);
+}
+}
+else
+{
+_putchar(format[i]);
+l++;
+}
+}
 va_end(argslist);
 return (l);
 }
