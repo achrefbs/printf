a895a1a64bab936237ab639ee7328e8c44ab01f4
achrefbs <achrefbs@github.com>
2020-03-17 17:47:01 +0000

    update

--- _printf.c
+++ _printf.c
@@ -4,9 +4,9 @@ int _printf(const char *format, ...)
 va_list argslist;
 int i, j, l = 0;
 specifiers_t specifiers[] = {
-{'c', print_char},
-{'s', print_string},
-{'d', print_integer}
+{"c", print_char},
+{"s", print_string},
+/*{'d', print_integer}*/
 };
 va_start(argslist, format);
 if (format == NULL)
@@ -14,30 +14,32 @@ return (-1);
 for (i = 0; format[i] != '\0'; i++)
 {
 	if (format[i] == '%' && format [i + 1] != '\0')
-		{
-			for (j = 0; j < 2; j++)
-			if (format[i + 1] == (specifiers + j)->dir)
+	{
+	for (j = 0; j < 2; j++)
+	{
+		if (format[i + 1] == (specifiers + j)->dir)
 			{
 				l += (specifiers + j)->f(argslist);
 				i += 1;
 			}
-			else if (format[i + 1] == '%')
+
+		else if (format[i + 1] == '%')
 			{
 				_putchar('%');
 				i += 1;
 				l += 1;
 			}
 		}
+	}
 	else if (format[i] == '%' && format[i + 1] == '\0')
 	{
 		return (-1);
 	}
-	
 	else
-		{
-			_putchar(format[i]);
-			l += 1;
-		}
+	{
+		_putchar(format[i]);
+		l += 1;
+	}
 }
 va_end(argslist);
 return (l);
