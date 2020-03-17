bc9f45e245b4dc0ac237ff0360f6fd1da1410239
vagrant <vagrant@homestead.test>
2020-03-17 22:29:38 +0000

    update

--- convert.c
+++ convert.c
@@ -4,33 +4,34 @@
 #include "holberton.h"
 int reverses(int n)
 {
-  int i, t = 0;
-  for(i = 0; n > 0;i++)
-  {t = (t * 10 )+ n%10;
-  n = (n - n%10) /10;}
-  return(t);
+int i, t = 0;
+for(i = 0; n > 0;i++)
+{
+t = (t * 10 )+ n%10;
+n = (n - n%10) /10;}
+return(t);
 }
 int print_number(int n)
 {
-  int i, l;
-  if (n < 0)
-  {
-  _putchar('-');
-  n = -n;
-  }
-  if (n == 0)
-  {
-  _putchar('0');
-  return (0);
-  }
-  n = reverses(n);
-  for (i = 0; n > 0; i++)
-  {
-    _putchar(n % 10 + '0');
-    n = n /10;
-    l++;
-  }
-  return (l);
+int i, l;
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+n = reverses(n);
+for (i = 0; n > 0; i++)
+{
+_putchar(n % 10 + '0');
+n = n /10;
+l++;
+}
+return (l);
 }
 int de_to_bi(va_list argslist)
 {
