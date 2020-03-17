3fd7a9872bb9bc79931ce3b8498e86c88b4b5642
vagrant <vagrant@homestead.test>
2020-03-17 22:44:38 +0000

    update integers

--- print_integer.c
+++ print_integer.c
@@ -3,32 +3,33 @@
 #include "holberton.h"
 int reverse(int n)
 {
-  int i, t = 0;
-  for(i = 0; n > 0;i++)
-  {t = (t * 10 )+ n%10;
-  n = (n - n%10) /10;}
-  return(t);
+int i, t = 0;
+for(i = 0; n > 0;i++)
+{
+t = (t * 10 )+ n%10;
+n = (n - n%10) /10;}
+return(t);
 }
 int print_integer(va_list argslist)
 {
-  int i, l = 0, n = 0;
-  n = va_arg(argslist, int);
-  if (n < 0)
-  {
-  _putchar('-');
-  n = -n;
-  }
-  if (n == 0)
-  {
-  _putchar('0');
-  return (0);
-  }
-  n = reverse(n);
-  for (i = 0; n > 0; i++)
-  {
-    _putchar(n % 10 + '0');
-    n = n /10;
-    l++;
-  }
-  return (l);
+int i, l = 0, n = 0;
+n = va_arg(argslist, int);
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+n = reverse(n);
+for (i = 0; n > 0; i++)
+{
+_putchar(n % 10 + '0');
+n = n /10;
+l++;
+}
+return (l);
 }
