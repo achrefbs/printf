e34723075b02648212f783d64aabe496ec0387b1
achrefbs <achrefbs@github.com>
2020-03-17 22:59:29 +0000

    update

--- print_integer.c
+++ print_integer.c
@@ -9,11 +9,21 @@
 int reverse(int n)
 {
 int i, t = 0;
-for(i = 0; n > 0;i++)
+for (i = 0; n > 0; i++)
 {
-t = (t * 10 )+ n%10;
-n = (n - n%10) /10;}
+t = (t * 10 )+ n % 10;
+n = (n - n % 10) / 10;}
 return(t);
+}
+int intlength(int n)
+{
+int l = 0;
+while (n != 0)
+{
+n /= 10;     
+++l;
+}
+return (l);
 }
 /**
  *print_integer- function to print integer
@@ -21,8 +31,9 @@ return(t);
  */
 int print_integer(va_list argslist)
 {
-int i, l = 0, n = 0;
+int i, l = 0, n = 0, c;
 n = va_arg(argslist, int);
+c = intlength(n);
 if (n < 0)
 {
 _putchar('-');
@@ -34,7 +45,7 @@ _putchar('0');
 return (0);
 }
 n = reverse(n);
-for (i = 0; n > 0; i++)
+for (i = 0; i < c; i++)
 {
 _putchar(n % 10 + '0');
 n = n /10;
