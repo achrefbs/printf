ca5a1a821275e8c4bbc2a367cd79d806c9280c59
vagrant <vagrant@homestead.test>
2020-03-17 22:34:40 +0000

    update

--- print_char.c
+++ print_char.c
@@ -2,8 +2,8 @@
 #include <stdarg.h>
 int print_char(va_list argslist)
 {
-	char c;
-	c = va_arg(argslist, int);
-	_putchar(c);
-	return (1);
+char c;
+c = va_arg(argslist, int);
+_putchar(c);
+return (1);
 }
