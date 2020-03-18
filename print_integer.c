55d5e2b7907e901f18b8c9ef608ccb33c6e4dfb7
vagrant <vagrant@homestead.test>
2020-03-18 17:10:24 +0000

    fix type of c

--- print_integer.c
+++ print_integer.c
@@ -9,25 +9,30 @@
  */
 int reverse(int n)
 {
-  int i, t = 0;
-  for (i = 0; n > 0; i++)
-    {
-      t = (t * 10) + n % 10;
-      n = (n - n % 10) / 10;
-    }
-  return (t);
+int i, t = 0;
+for (i = 0; n > 0; i++)
+{
+t = (t * 10) + n % 10;
+n = (n - n % 10) / 10;
+}
+return (t);
 }
 
+/**
+ *rev- function to reverse
+ *@c: character
+ *@i: integer
+ */
 void rev(char *c, int i)
 {
-  int j;
-  char tmp;
-  for (j = 0; j < (i / 2); j++)
-    {
-      tmp = c[j];
-      c[j] = c[i - j - 1];
-      c[i - j - 1] = tmp;
-    }
+int j;
+char tmp;
+for (j = 0; j < (i / 2); j++)
+{
+tmp = c[j];
+c[j] = c[i - j - 1];
+c[i - j - 1] = tmp;
+}
 }
 
 /**
@@ -37,13 +42,13 @@ void rev(char *c, int i)
  */
 int intlength(int n)
 {
-  int l = 0;
-  while (n != 0)
-    {
-      n /= 10;
-      ++l;
-    }
-  return (l);
+int l = 0;
+while (n != 0)
+{
+n /= 10;
+++l;
+}
+return (l);
 }
 /**
  *print_integer- function to print integer
@@ -52,28 +57,29 @@ int intlength(int n)
  */
 int print_integer(va_list argslist)
 {
-  int i, n = 0, c;
-  char *res;
-  n = va_arg(argslist, int);
-  c = intlength(n);
-  res = malloc(c + 1);
-  if (n < 0)
-    {
-      _putchar('-');
-      n = n *-1;
-    }
-  if (n == 0)
-    {
-      _putchar('0');
-      return (0);
-    }
-  for (i = 0; i < c; i++)
-    {
-      res[i] = (n % 10 + '0');
-      n = n / 10;
-    }
-  rev(res, c);
-  for (i = 0; i < c; i++)
-    _putchar(res[i]);
-  return (c);
+int i, n = 0;
+unsigned int long c;
+char *res;
+n = va_arg(argslist, int);
+c = intlength(n);
+res = malloc(c + 1);
+if (n < 0)
+{
+_putchar('-');
+n = n * -1;
+}
+if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+for (i = 0; i < c; i++)
+{
+res[i] = (n % 10 + '0');
+n = n / 10;
+}
+rev(res, c);
+for (i = 0; i < c; i++)
+_putchar(res[i]);
+return (c);
 }
