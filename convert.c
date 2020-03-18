b474d0a17eed6292ea713253cb219e25d83a299b
vagrant <vagrant@homestead.test>
2020-03-18 12:49:09 +0000

    update

--- convert.c
+++ convert.c
@@ -3,18 +3,19 @@
 #include <stdarg.h>
 #include "holberton.h"
 /**
- *reverse- function to reverse
+ *reverses- function to reverse
  *@n: integer
  *Return: return t
  */
 int reverses(int n)
 {
 int i, t = 0;
-for(i = 0; n > 0;i++)
+for (i = 0; n > 0; i++)
 {
-t = (t * 10 )+ n%10;
-n = (n - n%10) /10;}
-return(t);
+t = (t * 10) + n % 10;
+n = (n - n % 10) / 10;
+}
+return (t);
 }
 /**
  *print_number- function that print number
@@ -38,13 +39,14 @@ n = reverses(n);
 for (i = 0; n > 0; i++)
 {
 _putchar(n % 10 + '0');
-n = n /10;
+n = n / 10;
 l++;
 }
 return (l);
 }
 /**
- *de_to_bi- function that converts from decimal to binary 
+ *de_to_bi- function that converts from decimal to binary
+ *@argslist: list of arguments
  *Return: return 0
  */
 int de_to_bi(va_list argslist)
@@ -56,7 +58,7 @@ while (d != 0)
 {
 r = d % 2;
 d = d / 2;
-b = b + r * tmp;
+b = b + r *tmp;
 tmp = tmp * 10;
 }
 print_number(b);
