79bae5e7637e9a6c373e8e17eec0d93796a6da38
vagrant <vagrant@homestead.test>
2020-03-18 17:02:51 +0000

    fix integers

--- print_integer.c
+++ print_integer.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "holberton.h"
+#include <stdlib.h>
 /**
  *reverse- function that reverse an int
  *@n: integer
@@ -8,14 +9,27 @@
  */
 int reverse(int n)
 {
-int i, t = 0;
-for (i = 0; n > 0; i++)
+  int i, t = 0;
+  for (i = 0; n > 0; i++)
+    {
+      t = (t * 10) + n % 10;
+      n = (n - n % 10) / 10;
+    }
+  return (t);
+}
+
+void rev(char *c, int i)
 {
-t = (t * 10) + n % 10;
-n = (n - n % 10) / 10;
+  int j;
+  char tmp;
+  for (j = 0; j < (i / 2); j++)
+    {
+      tmp = c[j];
+      c[j] = c[i - j - 1];
+      c[i - j - 1] = tmp;
+    }
 }
-return (t);
-}
+
 /**
  *intlength- function that retunr length of integer
  *@n: integer
@@ -23,13 +37,13 @@ return (t);
  */
 int intlength(int n)
 {
-int l = 0;
-while (n != 0)
-{
-n /= 10;
-++l;
-}
-return (l);
+  int l = 0;
+  while (n != 0)
+    {
+      n /= 10;
+      ++l;
+    }
+  return (l);
 }
 /**
  *print_integer- function to print integer
@@ -38,24 +52,28 @@ return (l);
  */
 int print_integer(va_list argslist)
 {
-int i, n = 0, c;
-n = va_arg(argslist, int);
-c = intlength(n);
-if (n < 0)
-{
-_putchar('-');
-n = n * -1;
-}
-if (n == 0)
-{
-_putchar('0');
-return (0);
-}
-n = reverse(n);
-for (i = 0; i < c; i++)
-{
-_putchar(n % 10 + '0');
-n = n / 10;
-}
-return (c);
+  int i, n = 0, c;
+  char *res;
+  n = va_arg(argslist, int);
+  c = intlength(n);
+  res = malloc(c + 1);
+  if (n < 0)
+    {
+      _putchar('-');
+      n = n *-1;
+    }
+  if (n == 0)
+    {
+      _putchar('0');
+      return (0);
+    }
+  for (i = 0; i < c; i++)
+    {
+      res[i] = (n % 10 + '0');
+      n = n / 10;
+    }
+  rev(res, c);
+  for (i = 0; i < c; i++)
+    _putchar(res[i]);
+  return (c);
 }
